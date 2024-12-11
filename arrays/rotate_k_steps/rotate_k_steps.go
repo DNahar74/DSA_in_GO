699b5a97eb3d6e7e5868a6dd0e181cc5b05625f9
DNahar74 <nahardarsh55@gmail.com>
2024-12-12 00:29:46 +0530

    rotate_k_steps working in O(n)

--- arrays/rotate_k_steps/rotate_k_steps.go
+++ arrays/rotate_k_steps/rotate_k_steps.go
@@ -17,8 +17,9 @@ func RotateKSteps(list []int, k int) {
 	// 	list[0] = temp
 	// }
 
-	//? Approach-2: time - O(n) | space - O(1)
+	//? Approach-2: time - O(n^2) | space - O(1)
 
+	//* Just looks like O(n), it runs for n*k times
 	//* create a temp := last element in list
 	//* Run a loop where i goes in reverse across the list, until k == 0
 	//* In each iteration, swap list[i] and list[i-1]
@@ -26,21 +27,43 @@ func RotateKSteps(list []int, k int) {
 	//* list[i] = temp; reassign temp = last element; k--;
 	//* And, i = len(list) not len(list) - 1 because there is a decrement in i value after each loop, log to check if needed
 
-	// To remove the case of empty array
-	if len(list) < 1 {
+	// // To remove the case of empty array
+	// if len(list) < 1 {
+	// 	return
+	// }
+
+	// temp := list[len(list)-1]
+
+	// for i := len(list) - 1; k > 0; i-- {
+	// 	if i == 0 {
+	// 		list[i] = temp
+	// 		temp = list[len(list)-1]
+	// 		i = len(list)
+	// 		k--
+	// 	} else {
+	// 		list[i] = list[i-1]
+	// 	}
+	// }
+
+	//? Approach-3: time - O(n) | space - O(n)
+
+	//* Calculate the position for each index
+	//* NewPosition = (i + k)%len(list)
+	//* The array can be divided into 2 subarrays, 1 that don't change on %n and 2 that needs to circle back
+	//* There will be k elements that need to be circled back list[len(list)-k:]
+
+	if len(list) == 0 {
 		return
 	}
 
-	temp := list[len(list)-1]
-
-	for i := len(list) - 1; k > 0; i-- {
-		if i == 0 {
-			list[i] = temp
-			temp = list[len(list)-1]
-			i = len(list)
-			k--
-		} else {
-			list[i] = list[i-1]
-		}
+	k %= len(list)
+	circleBack := append([]int{}, list[len(list)-k:]...)
+	for i := len(list) - k - 1; i >= 0; i-- {
+		list[i+k] = list[i]
 	}
+
+	// This is reassignment, so it is local
+	// list = append(circleBack, list[k:]...)
+
+	copy(list[:k], circleBack)
 }
